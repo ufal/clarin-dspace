ed7a490198185dc5bf690803f90913a4c9dcf298
kuchtiak-ufal <kuchtiak@ufal.mff.cuni.cz>
2025-04-25 00:07:22 +0200

    Issue 1200: allow.edit.metadata property should also work for submitters that are members of collection SUBMIT subgroup (#1202)
    * Issue 1200: allow.edit.metadata property should also work for submitters that are members of collection SUBMIT subgroup
    
    * update-comment
    
    * improve the fix
    
    * checkstyle + formatting
    
    ---------
    
    Co-authored-by: Milan Kuchtiak <mkuchtiak@Milans-MacBook-Pro.local>

--- dspace-api/src/main/java/org/dspace/content/InstallItemServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/InstallItemServiceImpl.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -34,6 +33,7 @@ import org.dspace.core.Context;
 import org.dspace.discovery.IsoLangCodes;
 import org.dspace.embargo.service.EmbargoService;
 import org.dspace.eperson.EPerson;
+import org.dspace.eperson.service.GroupService;
 import org.dspace.event.Event;
 import org.dspace.identifier.Identifier;
 import org.dspace.identifier.IdentifierException;
@@ -63,6 +63,8 @@ public class InstallItemServiceImpl implements InstallItemService {
     protected IdentifierService identifierService;
     @Autowired(required = true)
     protected ItemService itemService;
+    @Autowired(required = true)
+    protected GroupService groupService;
     @Autowired(required = true)
     protected SupervisionOrderService supervisionOrderService;
     @Autowired(required = false)
@@ -114,7 +116,7 @@ public class InstallItemServiceImpl implements InstallItemService {
 
         //Allow submitter to edit item
         if (isCollectionAllowedForSubmitterEditing(item.getOwningCollection()) &&
-                isInSubmitGroup(item.getSubmitter(), item.getOwningCollection().getID())) {
+                isInSubmitGroup(c, item.getSubmitter(), item.getOwningCollection())) {
             createResourcePolicy(c, item, Constants.WRITE);
         }
 
@@ -379,17 +381,16 @@ public class InstallItemServiceImpl implements InstallItemService {
     }
 
     /**
-     * Checks if the given EPerson is in a submitter of the collection.
+     * Checks if the given ePerson is in the submit group of the collection.
      * A submit group is identified by the name containing "SUBMIT" and the collection UUID.
      *
-     * @param eperson      the EPerson whose is checked
-     * @param collectionUUID the UUID of the collection to check group names
-     * @return true if the EPerson is in a submitter, false otherwise
+     * @param context           The current DSpace context.
+     * @param ePerson           the EPerson that is checked to be member of the collection submit group
+     * @param collection        the collection
+     * @return true if the EPerson is a member (direct or indirect) of a submit group, false otherwise
      */
-    private boolean isInSubmitGroup(EPerson eperson, UUID collectionUUID) {
-        return eperson.getGroups().stream()
-                .anyMatch(group -> group.getName().contains("SUBMIT") &&
-                        group.getName().contains(collectionUUID.toString()));
+    private boolean isInSubmitGroup(Context context, EPerson ePerson, Collection collection) throws SQLException {
+        return groupService.isMember(context, ePerson, "COLLECTION_" + collection.getID() + "_SUBMIT");
     }
 
     /**
