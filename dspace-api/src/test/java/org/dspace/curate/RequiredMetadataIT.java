f32bd53403025e3a77f79ce81f4c9c7a94c3e6d7
Milan Kuchtiak <kuchtiak@ufal.mff.cuni.cz>
2025-07-28 09:09:00 +0200

    update test to test usecase with workspace item

--- dspace-api/src/test/java/org/dspace/curate/RequiredMetadataIT.java
+++ dspace-api/src/test/java/org/dspace/curate/RequiredMetadataIT.java
@@ -11,15 +11,26 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import org.dspace.AbstractIntegrationTestWithDatabase;
-import org.dspace.builder.CollectionBuilder;
-import org.dspace.builder.CommunityBuilder;
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.builder.ItemBuilder;
 import org.dspace.content.Collection;
+import org.dspace.content.Community;
 import org.dspace.content.Item;
+import org.dspace.content.WorkspaceItem;
+import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.CollectionService;
+import org.dspace.content.service.CommunityService;
+import org.dspace.content.service.ItemService;
+import org.dspace.content.service.WorkspaceItemService;
+import org.dspace.identifier.factory.IdentifierServiceFactory;
+import org.dspace.identifier.service.IdentifierService;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -30,6 +41,45 @@ import org.junit.Test;
 public class RequiredMetadataIT extends AbstractIntegrationTestWithDatabase {
     private static final String TASK_NAME = "requiredmetadata";
 
+    protected CommunityService communityService = ContentServiceFactory.getInstance().getCommunityService();
+    protected CollectionService collectionService = ContentServiceFactory.getInstance().getCollectionService();
+    protected ItemService itemService = ContentServiceFactory.getInstance().getItemService();
+    protected WorkspaceItemService workspaceItemService = ContentServiceFactory.getInstance().getWorkspaceItemService();
+    protected IdentifierService identifierService = IdentifierServiceFactory.getInstance().getIdentifierService();
+
+    Community parentCommunity;
+    Collection collection;
+    Item item1;
+    Item item2;
+    WorkspaceItem workspaceItem;
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        try {
+            //we have to create a new community in the database
+            context.turnOffAuthorisationSystem();
+            this.parentCommunity = communityService.create(null, context);
+            this.collection = collectionService.create(context, parentCommunity, "123456789/113");
+            this.workspaceItem = workspaceItemService.create(context, collection, true);
+            identifierService.register(context, workspaceItem.getItem());
+            item1 = ItemBuilder.createItem(context, collection)
+                    .withHandle("123456789/113-1")
+                    .withMetadata("dc", "title", null, "Test item")
+                    .withMetadata("dc", "date", "issued", "2025-07-23")
+                    .build();
+            item2 = ItemBuilder.createItem(context, collection)
+                    .withHandle("123456789/113-2")
+                    .build();
+            context.restoreAuthSystemState();
+        } catch (AuthorizeException ex) {
+            fail("Authorization Error in init: " + ex.getMessage());
+        } catch (SQLException ex) {
+            fail("SQL Error in init: " + ex.getMessage());
+        }
+    }
+
     @Test
     public void testPerform() throws IOException {
         Curator curator = new Curator();
@@ -38,41 +88,40 @@ public class RequiredMetadataIT extends AbstractIntegrationTestWithDatabase {
         curator.setReporter(reporter);
 
         context.setCurrentUser(admin);
-        parentCommunity = CommunityBuilder.createCommunity(context).build();
 
-        Collection collection = CollectionBuilder.createCollection(context, parentCommunity, "123456789/113")
-                                                 .build();
-        Item item1 = ItemBuilder.createItem(context, collection)
-                .withHandle("123456789/113-1")
-                .build();
-
-        Item item2 = ItemBuilder.createItem(context, collection)
-                .withHandle("123456789/113-2")
-                .withMetadata("dc", "title", null, "Test item")
-                .withMetadata("dc", "date", "issued", "2025-07-23")
-                .build();
-
-        String failResult =
-                "Item: 123456789/113-1 missing required field: dc.title. missing required field: dc.date.issued";
-        String successResult = "Item: 123456789/113-2 has all required fields";
-
-        // run curateTask for item1 - should fail
-        curator.curate(context, item1);
+        // run curateTask for workspaceItem - should fail
+        curator.curate(context, workspaceItem.getItem().getHandle());
         assertEquals("Curation should fail", Curator.CURATE_FAIL, curator.getStatus(TASK_NAME));
-        assertEquals(failResult, curator.getResult(TASK_NAME));
-        assertThat(reporter.getReport(), contains(failResult));
+        assertEquals(failResultForItem(workspaceItem.getItem()), curator.getResult(TASK_NAME));
+        assertThat(reporter.getReport(), contains(failResultForItem(workspaceItem.getItem())));
         reporter.getReport().clear();
 
-        // run curateTask for item2 - should pass
+        // run curateTask for item1 - should pass
+        curator.curate(context, item1);
+        assertEquals("Curation should succeed", Curator.CURATE_SUCCESS, curator.getStatus(TASK_NAME));
+        assertEquals(successResultForItem(item1), curator.getResult(TASK_NAME));
+        assertThat(reporter.getReport(), contains(successResultForItem(item1)));
+        reporter.getReport().clear();
+
+        // run curateTask for item2 - should fail
         curator.curate(context, item2);
-        assertEquals("Curation should succed", Curator.CURATE_SUCCESS, curator.getStatus(TASK_NAME));
-        assertEquals(successResult, curator.getResult(TASK_NAME));
-        assertThat(reporter.getReport(), contains(successResult));
+        assertEquals("Curation should fail", Curator.CURATE_FAIL, curator.getStatus(TASK_NAME));
+        assertEquals(failResultForItem(item2), curator.getResult(TASK_NAME));
+        assertThat(reporter.getReport(), contains(failResultForItem(item2)));
         reporter.getReport().clear();
 
         // run curateTask for collection
         curator.curate(context, collection);
-        assertThat(reporter.getReport(), containsInAnyOrder(failResult, successResult));
+        assertThat(reporter.getReport(), containsInAnyOrder(successResultForItem(item1), failResultForItem(item2)));
+    }
+
+    private static String failResultForItem(Item item) {
+        return "Item: " + item.getHandle()
+                + " missing required field: dc.title. missing required field: dc.date.issued";
+    }
+
+    private static String successResultForItem(Item item) {
+        return "Item: " + item.getHandle() + " has all required fields";
     }
 
 }
