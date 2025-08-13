92ba0dea445203b376e42619d0a931155f9ca52f
Milan Kuchtiak <kuchtiak@ufal.mff.cuni.cz>
2025-08-13 17:03:23 +0200

    attempt to fix unit tests

--- dspace-api/src/main/java/org/dspace/versioning/VersioningConsumer.java
+++ dspace-api/src/main/java/org/dspace/versioning/VersioningConsumer.java
@@ -137,7 +137,8 @@ public class VersioningConsumer implements Consumer {
 
         // unarchive previous item
         // the "versioning.unarchive.previous.version" property controls whether previous item is unarchived or not
-        if (configurationService.getBooleanProperty("versioning.unarchive.previous.version", true)) {
+        if (!latestItem.isArchived() ||
+                configurationService.getBooleanProperty("versioning.unarchive.previous.version", true)) {
             unarchiveItem(ctx, previousItem);
         }
 
