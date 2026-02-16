858f2edd283f6fdbb8a07c4e6ab52ed94fba0e57
Milan Kuchtiak <kuchtiak@ufal.mff.cuni.cz>
2026-02-16 16:10:54 +0100

    test must cleanup configuration property

--- dspace-api/src/test/java/org/dspace/ctask/general/CreateMissingIdentifiersIT.java
+++ dspace-api/src/test/java/org/dspace/ctask/general/CreateMissingIdentifiersIT.java
@@ -45,6 +45,11 @@ public class CreateMissingIdentifiersIT
         // Must remove any cached named plugins before creating a new one
         CoreServiceFactory.getInstance().getPluginService().clearNamedPluginClasses();
         // Define a new task dynamically
+
+        String oldPluginTask = configurationService.getProperty(P_TASK_DEF);
+
+        System.out.println("Old Plugin Task: " + oldPluginTask);
+
         configurationService.setProperty(P_TASK_DEF,
                 CreateMissingIdentifiers.class.getCanonicalName() + " = " + TASK_NAME);
 
@@ -82,5 +87,7 @@ public class CreateMissingIdentifiersIT
         curator.curate(context, item);
         int status = curator.getStatus(TASK_NAME);
         assertEquals("Curation should succeed", Curator.CURATE_SUCCESS, status);
+
+        configurationService.setProperty(P_TASK_DEF, oldPluginTask);
     }
 }
