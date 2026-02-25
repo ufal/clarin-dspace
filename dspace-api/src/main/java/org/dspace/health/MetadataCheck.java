1c395e77622b86f4f5a118e0c50aa091f96ea3c1
Milan Kuchtiak <kuchtiak@ufal.mff.cuni.cz>
2026-02-25 17:24:16 +0100

    improve MetadataCheck

--- dspace-api/src/main/java/org/dspace/health/MetadataCheck.java
+++ dspace-api/src/main/java/org/dspace/health/MetadataCheck.java
@@ -31,13 +31,17 @@ import org.json.JSONObject;
  */
 public class MetadataCheck extends Check {
 
-    private static final String QA_METADATA_ERRORS = "qa-metadata-errors.json";
+    private static final String QA_METADATA_ERROR_PATTERNS_JSON = "qa-metadata-error-patterns.json";
+    private static final String VALIDATION_TYPE_OTHER = "validation.other";
     private static final int COUNT_INDENTATION = 30;
     private static final int MAXIMUM_ERRORS_TO_SHOW = 50;
 
     private static Map<String, List<String>> errorPatterns;
-    private static Map<String, String> patternExamples;
+    private static Map<String, String> errorExamples;
     private static Map<String, Integer> errorCounts;
+    private static Map<String, List<String>> warningPatterns;
+    private static Map<String, String> warningExamples;
+    private static Map<String, Integer> warningCounts;
 
     @Override
     public String run(ReportInfo ri) {
@@ -50,9 +54,13 @@ public class MetadataCheck extends Check {
             error(e, "Cannot load error patterns");
         }
 
-        patternExamples = new HashMap<>();
+        errorExamples = new HashMap<>();
         // Use TreeMap to keep the error types sorted
         errorCounts = new TreeMap<>();
+
+        warningExamples = new HashMap<>();
+        // Use TreeMap to keep the warning types sorted
+        warningCounts = new TreeMap<>();
 
         Curator curator = new Curator();
         curator.addTask("metadataqa");
@@ -67,25 +75,72 @@ public class MetadataCheck extends Check {
         }
 
         List<String> report = reporter.getReport();
-        sb.append("Error Count: ").append(" ".repeat(COUNT_INDENTATION - "Error Count: ".length()))
-                .append(String.format("%7d", report.size())).append("\n");
 
-        errorCounts.forEach((key, val) -> {
-            String errorCode = formatErrorCode(key);
-            sb.append(errorCode).append(" ".repeat(COUNT_INDENTATION - errorCode.length()))
-                    .append(String.format("%7d",val)).append("\n");
-        });
+        int errorCount = errorCounts.values().stream().mapToInt(Integer::intValue).sum();
+        int warningCount = warningCounts.values().stream().mapToInt(Integer::intValue).sum();
 
-        if (report.size() > MAXIMUM_ERRORS_TO_SHOW) {
-            sb.append("\nError examples:\n");
-            patternExamples.forEach((key, val) -> sb.append(val.substring(val.indexOf("ERROR! "))).append("\n"));
-        } else {
-            sb.append("\nErrors:\n");
-            report.forEach(line -> sb.append(line.substring(line.indexOf("ERROR! "))).append("\n"));
+        // error statistics
+        if (errorCount > 0) {
+            sb.append("\nError statistics:\n\n");
+            errorCounts.forEach((key, val) -> {
+                String errorCode = formatErrorCode(key);
+                sb.append(errorCode).append(" ".repeat(COUNT_INDENTATION - errorCode.length()))
+                        .append(String.format("%7d", val)).append("\n");
+            });
+            sb.append("-".repeat(COUNT_INDENTATION + 7)).append("\n");
+            sb.append("Error count total: ")
+                    .append(" ".repeat(COUNT_INDENTATION - "Error count total: ".length()))
+                    .append(String.format("%7d", errorCount)).append("\n");
+        }
+
+        // warning statistics
+        if (warningCount > 0) {
+            sb.append("\nWarning statistics:\n\n");
+            warningCounts.forEach((key, val) -> {
+                String errorCode = formatErrorCode(key);
+                sb.append(errorCode).append(" ".repeat(COUNT_INDENTATION - errorCode.length()))
+                        .append(String.format("%7d", val)).append("\n");
+            });
+            sb.append("-".repeat(COUNT_INDENTATION + 7)).append("\n");
+            sb.append("Warning count total: ")
+                    .append(" ".repeat(COUNT_INDENTATION - "Warning count total: ".length()))
+                    .append(String.format("%7d", warningCount)).append("\n");
+        }
+
+        // list of errors
+        if (errorCount > 0) {
+            if (errorCount > MAXIMUM_ERRORS_TO_SHOW) {
+                sb.append("\nError examples:\n");
+                errorExamples.forEach((key, val) -> sb.append(val.substring(val.indexOf("ERROR! "))).append("\n"));
+            } else {
+                sb.append("\nErrors:\n");
+                report.forEach(line -> {
+                    if (line.contains("ERROR! ")) {
+                        sb.append(line.substring(line.indexOf("ERROR! "))).append("\n");
+                    }
+                });
+            }
+        }
+
+        // list of warnings
+        if (warningCount > 0) {
+            if (warningCount > MAXIMUM_ERRORS_TO_SHOW) {
+                sb.append("\nWarning examples:\n");
+                warningExamples.forEach((key, val) -> sb.append(val.substring(val.indexOf("Warning: "))).append("\n"));
+            } else {
+                sb.append("\nWarnings:\n");
+                report.forEach(line -> {
+                    if (!line.contains("ERROR! ") && line.contains("Warning: ")) {
+                        sb.append(line.substring(line.indexOf("Warning: "))).append("\n");
+                    }
+                });
+            }
         }
 
         // populate report with error counts
-        root.put("errorCount", report.size());
+        root.put("errorCount", errorCount);
+        root.put("warningCount", warningCount);
+
         JSONArray errors = new JSONArray();
         errorCounts.forEach((key, val) -> {
             JSONObject error = new JSONObject()
@@ -94,6 +149,18 @@ public class MetadataCheck extends Check {
             errors.put(error);
         });
         root.put("errors", errors);
+
+        JSONArray warnings = new JSONArray();
+        warningCounts.forEach((key, val) -> {
+            JSONObject warning = new JSONObject()
+                    .put("type", key)
+                    .put("count", val);
+            warnings.put(warning);
+        });
+
+        root.put("errors", errors);
+        root.put("warnings", warnings);
+
         this.setReportJson(root);
         return sb.toString();
     }
@@ -114,7 +181,10 @@ public class MetadataCheck extends Check {
             String line = cs.toString();
             if (line.contains("ERROR! ")) {
                 report.add(line);
-                populateData(line, errorPatterns, errorCounts, patternExamples);
+                populateData("ERROR! ", line, errorPatterns, errorCounts, errorExamples);
+            } else if (line.contains("Warning: ")) {
+                report.add(line);
+                populateData("Warning: ", line, warningPatterns, warningCounts, warningExamples);
             }
             return this;
         }
@@ -133,14 +203,15 @@ public class MetadataCheck extends Check {
 
     private void loadPatterns() throws IOException {
         InputStream qaMetadataErrors = Thread.currentThread()
-                .getContextClassLoader().getResourceAsStream(QA_METADATA_ERRORS);
+                .getContextClassLoader().getResourceAsStream(QA_METADATA_ERROR_PATTERNS_JSON);
         JsonNode root = new ObjectMapper().readTree(qaMetadataErrors);
 
         // Load error types and their associated error messages
-        errorPatterns = getValidationPatterns(root.withObject("errors"));
+        errorPatterns = getPatterns(root.withObject("errors"));
+        warningPatterns = getPatterns(root.withObject("warnings"));
     }
 
-    private Map<String, List<String>> getValidationPatterns(JsonNode parentNode) {
+    private Map<String, List<String>> getPatterns(JsonNode parentNode) {
         Map<String, List<String>> validationPatterns = new HashMap<>();
         parentNode.fieldNames().forEachRemaining(validationType -> {
             List<String> validationMessages = new ArrayList<>();
@@ -154,25 +225,26 @@ public class MetadataCheck extends Check {
         return validationPatterns;
     }
 
-    private static void populateData(String line,
-                                     Map<String, List<String>> errorPatterns,
-                                     Map<String, Integer> errorCounts,
-                                     Map<String, String> patternExamples) {
-        String errorMessage = line.substring(line.indexOf("ERROR! ") + 7, line.indexOf("[[") - 1);
-        for (Map.Entry<String, List<String>> entry : errorPatterns.entrySet()) {
-            String validationType = entry.getKey();
-            List<String> patterns = entry.getValue();
-            boolean found = false;
-            for (String pattern : patterns) {
+    private static void populateData(String prefix,
+                                     String line,
+                                     Map<String, List<String>> patterns,
+                                     Map<String, Integer> counts,
+                                     Map<String, String> examples) {
+        String message = line.substring(line.indexOf(prefix) + prefix.length(), line.indexOf("[[") - 1);
+        boolean found = false;
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            String type = entry.getKey();
+            List<String> typePatterns = entry.getValue();
+            for (String pattern : typePatterns) {
                 boolean startsWithCaret = pattern.startsWith("^");
                 boolean endsWithDollar = pattern.endsWith("$");
-                if ((startsWithCaret && errorMessage.startsWith(pattern.substring(1))) ||
-                        (endsWithDollar && errorMessage.endsWith(pattern.substring(0, pattern.length() - 1))) ||
-                        (!startsWithCaret && !endsWithDollar && errorMessage.contains(pattern))
+                if ((startsWithCaret && message.startsWith(pattern.substring(1))) ||
+                        (endsWithDollar && message.endsWith(pattern.substring(0, pattern.length() - 1))) ||
+                        (!startsWithCaret && !endsWithDollar && message.contains(pattern))
                 ) {
                     // increase the count for this validation type
-                    errorCounts.merge(validationType, 1, Integer::sum);
-                    patternExamples.putIfAbsent(errorMessage, line);
+                    counts.merge(type, 1, Integer::sum);
+                    examples.putIfAbsent(message, line);
                     found = true;
                     break;
                 }
@@ -181,14 +253,19 @@ public class MetadataCheck extends Check {
                 break; // If a pattern is found, no need to check other patterns for this message
             }
         }
+        if (!found) {
+            // If no pattern matched, categorize under "validation.other"
+            counts.merge(VALIDATION_TYPE_OTHER, 1, Integer::sum);
+            examples.putIfAbsent(message, line);
+        }
     }
 
     private static String formatErrorCode(String errorCode) {
         if (errorCode.startsWith("validation.")) {
             String validationCode = errorCode.substring("validation.".length());
-            return validationCode.replaceAll("\\.", " ") + " errors: ";
+            return validationCode.replaceAll("\\.", " ") + " issues: ";
         } else {
-            return errorCode + " errors: ";
+            return errorCode + " issues: ";
         }
     }
 }
