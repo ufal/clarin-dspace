81cb91441e18708461b4cfdf597538398c41bb39
Milan Kuchtiak <kuchtiak@ufal.mff.cuni.cz>
2026-03-19 11:35:17 +0100

    Issue 1346: signposting/describedby endpoint returns incorrect XML file

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/signposting/controller/LinksetRestController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/signposting/controller/LinksetRestController.java
@@ -163,7 +163,8 @@ public class LinksetRestController {
         DSpaceObject object = findObject(context, uuid);
         DisseminationCrosswalk xwalk = (DisseminationCrosswalk)
                 pluginService.getNamedPlugin(DisseminationCrosswalk.class, xwalkName);
-        List<Element> elements = xwalk.disseminateList(context, object);
+        // Output valid XML: disseminate using root element as opposed to list
+        Element elements = xwalk.disseminateElement(context, object);
         XMLOutputter outputter = new XMLOutputter(Format.getCompactFormat());
         return outputter.outputString(elements);
     }
