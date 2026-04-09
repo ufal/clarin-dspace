6f5941a6c48ff4cbec7bb8bf7546a7e08aa85525
Milan Kuchtiak <kuchtiak@ufal.mff.cuni.cz>
2026-04-09 10:07:20 +0200

    more debug messages

--- dspace-api/src/main/java/org/dspace/iiif/consumer/IIIFCacheEventConsumer.java
+++ dspace-api/src/main/java/org/dspace/iiif/consumer/IIIFCacheEventConsumer.java
@@ -102,18 +102,23 @@ public class IIIFCacheEventConsumer implements Consumer {
 
         switch (et) {
             case Event.ADD:
+                System.out.println("ADD event for subject " + subject + ", adding to cache eviction");
                 addToCacheEviction(subject, unmodifiedSubject, st);
                 break;
             case Event.MODIFY:
+                System.out.println("MODIFY event for subject " + subject + ", adding to cache eviction");
                 addToCacheEviction(subject, unmodifiedSubject, st);
                 break;
             case Event.MODIFY_METADATA:
+                System.out.println("MODIFY_METADATA event for subject " + subject + ", adding to cache eviction");
                 addToCacheEviction(subject, unmodifiedSubject, st);
                 break;
             case Event.REMOVE:
+                System.out.println("REMOVE event for subject " + subject + ", adding to cache eviction");
                 addToCacheEviction(subject, unmodifiedSubject, st);
                 break;
             case Event.DELETE:
+                System.out.println("DELETE event for subject " + subject + ", adding to cache eviction");
                 addToCacheEviction(subject, unmodifiedSubject, st);
                 break;
             default: {
@@ -126,6 +131,12 @@ public class IIIFCacheEventConsumer implements Consumer {
     private void addToCacheEviction(DSpaceObject subject, DSpaceObject subject2, int type) {
         if (type == Constants.BITSTREAM) {
             toEvictFromCanvasCache.add(subject2);
+        }
+        if (subject == null) {
+            System.out.println("DSpaceObject is null");
+            Thread.dumpStack();
+        } else {
+            System.out.println("Adding DSpaceObject with name '" + subject.getName() + "' to eviction list");
         }
         toEvictFromManifestCache.add(subject);
     }
@@ -142,8 +153,8 @@ public class IIIFCacheEventConsumer implements Consumer {
             } else {
                 for (DSpaceObject dso : toEvictFromManifestCache) {
                     if (dso == null) {
-                        System.out.println("DSpaceObject is null, skipping eviction from manifest cache");
-                        continue;
+                        System.out.println("Method:end - DSpaceObject is null");
+                        // continue;
                     }
                     UUID uuid = dso.getID();
                     manifestsCacheEvictService.evictSingleCacheValue(uuid.toString());
