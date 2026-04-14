b913627804b3f58c50788139438a7fef536c6841
Milan Kuchtiak <kuchtiak@ufal.mff.cuni.cz>
2026-04-14 14:06:51 +0200

    Issue 1349: admin user is not allowed to delete himself/herself (ufal/clarin-dspace#1350)
    * Issue 1349: admin user is not allowed to delete himself/herself
    
    * fixed comment
    
    * resolve Copilot comments
    
    * improve the fix: test context.getCurrentUser() for null
    
    * throw IllegalStateException rather than AuthorizeException, and allow client to see the error message

--- dspace-api/src/main/java/org/dspace/eperson/EPersonServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/eperson/EPersonServiceImpl.java
@@ -376,6 +376,14 @@ public class EPersonServiceImpl extends DSpaceObjectServiceImpl<EPerson> impleme
             throw new AuthorizeException(
                     "You must be an admin to delete an EPerson");
         }
+        // Admin cannot delete himself/herself
+        if (!context.ignoreAuthorization()) {
+            EPerson currentUser = context.getCurrentUser();
+            if (currentUser != null && ePerson.getID().equals(currentUser.getID())) {
+                throw new IllegalStateException(
+                        "You, as admin user, cannot delete yourself");
+            }
+        }
         // Get all workflow-related groups that the current EPerson belongs to
         Set<Group> workFlowGroups = getAllWorkFlowGroups(context, ePerson);
         for (Group group: workFlowGroups) {
